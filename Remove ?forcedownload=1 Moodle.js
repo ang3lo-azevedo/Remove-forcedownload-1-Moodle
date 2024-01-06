c24e10eb243dd0d97a6fecbfb76a8222146f32e8
Ângelo Azevedo <66081804+ang3lo-azevedo@users.noreply.github.com>
2024-01-06 17:20:32 +0000

    Update Remove ?forcedownload=1 Moodle.js

--- Remove ?forcedownload=1 Moodle.js
+++ Remove ?forcedownload=1 Moodle.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         Remove ?forcedownload=1 Moodle
 // @version      1.1
-// @description  Remove o parâmetro ?forcedownload=1 no Moodle do ISEL e abre documentos numa nova tab
-// Não façam download de jogos pirateados a partir de sites brasileiros se faz favor. Usem o r/Piracy.
+// @description  Remove o parâmetro ?forcedownload=1 no Moodle do ISEL e abre documentos numa nova tab. Não façam download de jogos pirateados a partir de sites brasileiros se faz favor. Usem o r/Piracy.
 // @author       Ângelo Azevedo
 // @match        https://2324moodle.isel.pt/*
 // @downloadURL  https://raw.githubusercontent.com/ang3lo-azevedo/Remove-forcedownload-1-Moodle/main/Remove%20%3Fforcedownload%3D1%20Moodle.js
